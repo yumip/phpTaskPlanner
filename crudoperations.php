0131060d4f7aa8f15a1892d935d3788c15f86338
Yumi Patton <yumipatton@gmail.com>
2021-03-04 13:49:27 +1100

    fix

--- crudoperations.php
+++ crudoperations.php
@@ -93,40 +93,40 @@ function displayTask($conn,$getStatus){
           <div data-task class="task">
             <div  class="row">
               <div class = "col-lg-6 order-2 order-lg-1">
-                <a href="#newTaskInput" data-editbutton role="button" class="d-inline text-decoration-none btn btn-link col-2 ml-0 pl-0 text-dark" data-toggle="modal" data-target="#newTaskInput" data-id='.$row[task_id].'>
+                <a href="#newTaskInput" data-editbutton role="button" class="d-inline text-decoration-none btn btn-link col-2 ml-0 pl-0 text-dark" data-toggle="modal" data-target="#newTaskInput" data-id='.$row["task_id"].'>
                     <i class="fas fa-edit text-secondary"></i>
                 </a>
-                  <p class = "text-left d-inline"> <span class="h5" data-name="tasktitle">'.$row[task_title].'</span> 
-                  <a href="#task'.$row[task_id].'Description" class="text-secondary icon ml-0 pl-0 small" data-toggle="collapse" data-target="#task'.$row[task_id].'Description"> See More
+                  <p class = "text-left d-inline"> <span class="h5" data-name="tasktitle">'.$row["task_title"].'</span> 
+                  <a href="#task'.$row["task_id"].'Description" class="text-secondary icon ml-0 pl-0 small" data-toggle="collapse" data-target="#task'.$row["task_id"].'Description"> See More
                 </a></p>
               </div>
               <div class="col-lg-6 order-1 order-lg-2">
                 <ul class = "row justify-content-between taskSummary" >
                   <li class="col-4 col-sm-1 order-1 order-sm-1">
-                  <i class = "fas fa-exclamation-triangle '.$row[task_priority].'" data-toggle = "tooltip" data-placement = "top" title = "Priority"></i>
+                  <i class = "fas fa-exclamation-triangle '.$row["task_priority"].'" data-toggle = "tooltip" data-placement = "top" title = "Priority"></i>
                   </li>
                   <li class="col-4 col-sm-1 order-2 order-sm-2 text-center">
-                    <i class="icon fas fa-star '.$row[task_status].'" data-toggle="tooltip" data-placement="top" title="Status"></i>
+                    <i class="icon fas fa-star '.$row["task_status"].'" data-toggle="tooltip" data-placement="top" title="Status"></i>
                   </li>
                   <li class="col-6 col-sm-4 order-4 order-sm-3 text-sm-right">
-                    '.$row[task_date].' '.$row[task_time].'
+                    '.$row["task_date"].' '.$row["task_time"].'
                   </li>
                   <li class="col-6 col-sm-4 order-5 order-sm-4 text-sm-center text-right" data-name="taskassignee">
-                  '.$row[task_assignee].'
+                  '.$row["task_assignee"].'
                   </li>
                   <li class="col-4 col-sm-1 order-3 order-sm-5 text-right">
-                      <a href="index.php?del_task_id='.$row[task_id].'" type="button" class="bin ml-0 pl-0 btn btn-link text-secondary"><i class="icon fas fa-trash"></i></a>
+                      <a href="index.php?del_task_id='.$row["task_id"].'" type="button" class="bin ml-0 pl-0 btn btn-link text-secondary"><i class="icon fas fa-trash"></i></a>
                   </li>
                 </ul>
               </div>
             </div>
-            <div id="task'.$row[task_id].'Description" class="collapse" data-name="taskdescription">'.$row[task_description].'</div>
+            <div id="task'.$row["task_id"].'Description" class="collapse" data-name="taskdescription">'.$row["task_description"].'</div>
             <hr>
              <input type = "hidden" 
-             data-taskDate = '.$row[task_date].'
-             data-taskTime = '.$row[task_time].'
-             data-taskPriority ='.$row[task_priority].'
-             data-taskStatus ='.$row[task_status].'>
+             data-taskDate = '.$row["task_date"].'
+             data-taskTime = '.$row["task_time"].'
+             data-taskPriority ='.$row["task_priority"].'
+             data-taskStatus ='.$row["task_status"].'>
           </div>';
        }
    }
